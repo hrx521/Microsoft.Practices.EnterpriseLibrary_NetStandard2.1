b8ec46f03844c8f842ca46f0044240762f3010d1
大风 <31436282@qq.com>
2021-08-16 19:31:00 +0800

    添加项目文件。

---
+++ Src/Data/Configuration/Fluent/DatabaseProviderExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data.Configuration.Fluent;
+
+namespace Microsoft.Practices.EnterpriseLibrary.Common.Configuration
+{
+    ///<summary>
+    /// Provides extensions for common database providers.
+    ///</summary>
+    public static class DatabaseProviderExtensions
+    {
+        ///<summary>
+        /// A Sql database for use with the System.Data.SqlClient namespace.
+        ///</summary>
+        /// <param name="context">Configuration context</param>
+        ///<returns></returns>
+        /// <seealso cref="System.Data.SqlClient"/>
+        public static IDatabaseSqlDatabaseConfiguration ASqlDatabase(this IDatabaseConfigurationProviders context)
+        {
+            return new SqlDatabaseConfigurationExtension(context);
+        }
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
+        public static IDatabaseAnotherDatabaseConfiguration AnotherDatabaseType(this IDatabaseConfigurationProviders context, string providerName)
+#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
+        {
+            return new AnotherDatabaseConfigurationExtensions(context, providerName);
+        }
+    }
+}
